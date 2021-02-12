5abf63687ac6fd07c0588c82525d50f8affdb064
thaistardy - Trabalho <thaistardy.s@gmail.com>
2021-02-12 11:52:28 -0300

    Adicionando todos os arquvis prontos!

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sejam Bem vindos!
+
+## Vamos inciar um novo Projeto de Estudo!
+
+[Clique aqui](https://athaistardy.github.io/projetostarbucks/) e Veja a Página em funcionamento!
---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function menuToggle() {
+    let menuArea = document.getElementById("menu-area");
+    if (menuArea.style.display == 'none') {
+        menuArea.style.display = 'block'
+    } else {
+        menuArea.style.display = 'none'
+    }
+
+}
